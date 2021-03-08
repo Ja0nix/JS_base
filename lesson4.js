5e4ae1893251ffa6b4eaeac7cf5f4da1c024f925
Ja0nix <62944305+Ja0nix@users.noreply.github.com>
2021-03-08 16:49:24 +0300

    task 2

--- lesson4.js
+++ lesson4.js
@@ -28,3 +28,35 @@ function numberToObject(num) {
 2.1. В прошлом домашнем задании вы реализовали корзину на базе массивов. Какими объектами можно заменить их элементы?
 2.2. Реализуйте такие объекты.
 2.3. Перенести функционал подсчета корзины на объектно-ориентированную базу.*/
+
+//Корзина с объектами в ней
+let cart = [
+    {
+        name: 'item1',
+        price: 180,
+        amount: 2
+    },
+    {
+        name: 'item2',
+        price: 200,
+        amount: 3
+    }
+]
+
+// подсчет итоговой суммы
+let sum = 0;
+for(item in cart)
+    {
+      sum += cart[item].price * cart[item].amount;
+    }
+console.log(sum);
+
+// или подсчет суммы итоговой в виде функции
+function totalSum(cart) {
+    let sum = 0;
+    for(item in cart)
+        {
+        sum += cart[item].price * cart[item].amount;
+        }
+    return sum;
+}
